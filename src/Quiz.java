5b4e9430ea80b3ac2fb9c7342173c281ad41417e
Allenkc <aa87220@gmail.com>
2020-09-01 22:47:34 +0800

    [add] 把sampleInput往外一層放才讀的到

--- src/Quiz.java
+++ src/Quiz.java
@@ -1,9 +1,27 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 public class Quiz {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        if(null == args[0]){
+            throw new IllegalArgumentException();
+        }
+
+        File file = new File(".");
+        // List Files
+//        for(String fileNames : file.list()) System.out.println(fileNames);
 
         String fileName = args[0];
+        BufferedReader reader = new BufferedReader(new FileReader(fileName));
 
-        System.out.println("Input File : " + fileName);
+        String line = reader.readLine();
+
+        while(line!=null){
+            System.out.println(line);
+            line = reader.readLine();
+        }
     }
 }
