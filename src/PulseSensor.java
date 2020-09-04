cb59f992af304f80ec522b5e2a2673593136a995
Allenkc <aa87220@gmail.com>
2020-09-04 22:58:52 +0800

    [rft] 抽成abstract

--- src/PulseSensor.java
+++ src/PulseSensor.java
@@ -4,19 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PulseSensor implements Sensor {
-
-    private String sensorType;
-    private String deviceName;
-    private String factorDataSetFileName;
-    private double safeRangeLowerBound;
-    private double safeRangeUpperBound;
-    private String isAttachedTo; //which patient
-    private List<Double> factorValue;
-    private int detectCounter;
-
-    public PulseSensor() {
-    }
+public class PulseSensor extends AbstractSensor {
 
     public PulseSensor(String deviceName, String factorDataSetFileName, double safeRangeLowerBound, double safeRangeUpperBound, String isAttachedTo, String sensorType) {
         this.deviceName = deviceName;
@@ -27,106 +15,4 @@ public class PulseSensor implements Sensor {
         this.detectCounter = 0;
         this.sensorType = sensorType;
     }
-
-    public String getDeviceName() {
-        return deviceName;
-    }
-
-    public void setDeviceName(String deviceName) {
-        this.deviceName = deviceName;
-    }
-
-    public String getFactorDataSetFileName() {
-        return factorDataSetFileName;
-    }
-
-    public void setFactorDataSetFileName(String factorDataSetFileName) {
-        this.factorDataSetFileName = factorDataSetFileName;
-    }
-
-    public double getSafeRangeLowerBound() {
-        return safeRangeLowerBound;
-    }
-
-    public void setSafeRangeLowerBound(double safeRangeLowerBound) {
-        this.safeRangeLowerBound = safeRangeLowerBound;
-    }
-
-    public double getSafeRangeUpperBound() {
-        return safeRangeUpperBound;
-    }
-
-    public void setSafeRangeUpperBound(double safeRangeUpperBound) {
-        this.safeRangeUpperBound = safeRangeUpperBound;
-    }
-
-    public String getIsAttachedTo() {
-        return isAttachedTo;
-    }
-
-    public void setIsAttachedTo(String isAttachedTo) {
-        this.isAttachedTo = isAttachedTo;
-    }
-
-    public List<Double> getFactorValue() {
-        return factorValue;
-    }
-
-    public void setFactorValue(List<Double> factorValue) {
-        this.factorValue = factorValue;
-    }
-
-    public int getDetectCounter() {
-        return detectCounter;
-    }
-
-    public void addDetectCounter() {
-        this.detectCounter += 1;
-    }
-
-    @Override
-    public void alarm(String patientName, String deviceName, double factorValue) {
-        System.out.print(patientName + " is in danger!" + " Cause: " + deviceName + " " + factorValue);
-    }
-
-    @Override
-    public void readFactorValue() throws IOException {
-
-        BufferedReader reader = new BufferedReader(new FileReader(factorDataSetFileName));
-
-        String line = reader.readLine();
-        factorValue = new ArrayList<>();
-        factorValue.add(Double.parseDouble(line));
-        while (line != null) {
-            line = reader.readLine();
-
-            if (line != null) {
-                factorValue.add(Double.parseDouble(line));
-            }
-
-        }
-
-    }
-
-    @Override
-    public double showValue() {
-
-        double nowValue = -1;
-
-        if (detectCounter <= factorValue.size() - 1) {
-            nowValue = factorValue.get(detectCounter);
-        }
-
-        return nowValue;
-    }
-
-    public void incrementCounter() {
-        detectCounter++;
-    }
-
-    @Override
-    public String getSensorTypeName() {
-        return this.sensorType;
-    }
-
 }
--- src/Sensor.java
+++ src/Sensor.java
@@ -1,12 +1,10 @@
 import java.io.IOException;
 
 public interface Sensor {
-    void alarm(String patientName , String deviceName , double factorValue);
+    void alarm(Long index , String patientName, String deviceName, double factorValue);
     String getDeviceName();
-    String getFactorDataSetFileName();
     double getSafeRangeLowerBound();
     double getSafeRangeUpperBound();
-    String getIsAttachedTo();
     void readFactorValue() throws IOException;
     double showValue();
     void incrementCounter();
